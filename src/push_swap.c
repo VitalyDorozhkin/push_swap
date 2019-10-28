a540af1a9ce2997c5008007dca2b02e81808585d
VitalyDorozhkin <dorozhkin.vitaly@gmail.com>
2019-10-29 02:37:27 +0300

    OXUENNO

--- src/push_swap.c
+++ src/push_swap.c
@@ -425,7 +425,6 @@ void quick_sort(t_list **lst_a, t_list **lst_b)
 {
 	int midi = mid(*lst_a);
 	int lena = ft_lstlen(*lst_a);
-	int lenb = 0;
 	int i = 0;
 	while ((lena = ft_lstlen(*lst_a)) > 3)
 	{
@@ -436,7 +435,7 @@ void quick_sort(t_list **lst_a, t_list **lst_b)
 			if((*lst_a)->content_size < midi && ++i)
 			{
 				pb(lst_a, lst_b, 1);
-				if(lst_b < mid(*lst_b))
+				if((*lst_b)->content_size > mid(*lst_b))
 				{
 					if(i < lena / 2 && lena - i > 3 && (*lst_a)->content_size >= midi)
 						rr(lst_a, lst_b, 1);
@@ -450,101 +449,6 @@ void quick_sort(t_list **lst_a, t_list **lst_b)
 	}
 }
 
-
-
-
-
-/*
-void quickSort(int *numbers, int left, int right)
-{
-  int pivot; // разрешающий элемент
-  int l_hold = left; //левая граница
-  int r_hold = right; // правая граница
-  pivot = numbers[left];
-  while (left < right) // пока границы не сомкнутся
-  {
-    while ((numbers[right] >= pivot) && (left < right))
-      right--; // сдвигаем правую границу пока элемент [right] больше [pivot]
-    if (left != right) // если границы не сомкнулись
-    {
-      numbers[left] = numbers[right]; // перемещаем элемент [right] на место разрешающего
-      left++; // сдвигаем левую границу вправо
-    }
-    while ((numbers[left] <= pivot) && (left < right))
-      left++; // сдвигаем левую границу пока элемент [left] меньше [pivot]
-    if (left != right) // если границы не сомкнулись
-    {
-      numbers[right] = numbers[left]; // перемещаем элемент [left] на место [right]
-      right--; // сдвигаем правую границу вправо
-    }
-  }
-  numbers[left] = pivot; // ставим разрешающий элемент на место
-  pivot = left;
-  left = l_hold;
-  right = r_hold;
-  if (left < pivot) // Рекурсивно вызываем сортировку для левой и правой части массива
-    quickSort(numbers, left, pivot - 1);
-  if (right > pivot)
-    quickSort(numbers, pivot + 1, right);
-}
-
-
-int	*get_sorted(t_list *lst)
-{
-	int	*res;
-	int	i;
-	int len;
-	t_list *tmp;
-
-	tmp = lst;
-	len = ft_lstlen(tmp);
-	i = 0;
-	res = ft_memalloc(sizeof(int) * len);
-	while (i < len)
-	{
-		res[i++] = tmp->content_size;
-		tmp = tmp->next;
-	}
-	quickSort(res, 0, len - 1);
-	return (res);
-}
-
-
-void    handle_a(t_list **lst_a, t_list **lst_b)
-{
-    int     *arr;
-    int     i;
-    int     j;
-    t_list   *tmp;
-    arr = get_sorted(*lst_a);
-    i = ft_lstlen(*lst_a);
-    j = 0;
-    while (i > 3)
-    {
-        j = i;
-        while ((j-- > 0))
-        {
-            tmp = *lst_a;
-            if (tmp->content_size > arr[(3 * i) / 4 - 1])
-            {
-                pb(lst_a, lst_b, 1);
-                rb(lst_b, 1);
-            }
-            else if (tmp->content_size > arr[i / 2 - 1])
-                pb(lst_a, lst_b, 1);
-            else
-                ra(lst_a, 1);
-        }
-        j = i / 2;
-        while (j-- > 0)
-            rra(lst_a, 1);
-        i = i / 2;
-        j = 10;
-    }
-    while (i-- > 0)
-        ra(lst_a, 1);
-}
-*/
 
 int		main(int argc, char **argv)
 {
